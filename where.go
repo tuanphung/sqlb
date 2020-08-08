41f11e68480649510dcca957c0b3b514a876140b
Tuan <tuanphunglk@gmail.com>
2020-08-08 14:46:08 +0800

    Allow chain Raw from Where

--- where.go
+++ where.go
@@ -6,6 +6,10 @@ func (c WhereChain) ToExpr() (string, []interface{}, error) {
 	return Chain(c).ToExpr()
 }
 
+func (c WhereChain) Raw(raw string, args ...interface{}) RawChain {
+	return ChainBuilder{Chain(c)}.Raw(raw, args)
+}
+
 func (c WhereChain) Limit(limit int64) LimitChain {
 	return ChainBuilder{Chain(c)}.Limit(limit)
 }
