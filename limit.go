3150912951d4e3bbab4d89255b67657d0ec16fea
Tuan <tuanphunglk@gmail.com>
2020-08-15 16:26:05 +0800

    Allow OrderBy after Limit

--- limit.go
+++ limit.go
@@ -9,3 +9,7 @@ func (c LimitChain) ToExpr() (string, []interface{}, error) {
 func (c LimitChain) Offset(offset int64) OffsetChain {
 	return ChainBuilder{Chain(c)}.Offset(offset)
 }
+
+func (c LimitChain) OrderBy(orders ...Order) OrderByChain {
+	return ChainBuilder{Chain(c)}.OrderBy(orders...)
+}
