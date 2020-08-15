500e9272c327b221cd696bfed93bbfdf991d32ff
Tuan <tuanphunglk@gmail.com>
2020-08-15 16:29:26 +0800

    Chain Offset with OrderBy

--- order_by.go
+++ order_by.go
@@ -9,3 +9,7 @@ func (c OrderByChain) ToExpr() (string, []interface{}, error) {
 func (c OrderByChain) Raw(raw string, args ...interface{}) RawChain {
 	return ChainBuilder{Chain(c)}.Raw(raw, args...)
 }
+
+func (c OrderByChain) Offset(offset int64) OffsetChain {
+	return ChainBuilder{Chain(c)}.Offset(offset)
+}
